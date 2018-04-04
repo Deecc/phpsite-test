3cf88c674b8594ed65e7afe78e3f8168d08712bb
decio <decio.crf@gmail.com>
2018-04-04 12:48:57 -0300

    adicao de logout

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <h2>Decio Casagrande</h2>
 
 <?php if (isLoggedIn()): ?>
-    <p> Olá, <?php echo $_SESSION['user_name']; ?> . </p>
+    <p> Olá, <?php echo $_SESSION['user_name']; ?> . | <a href="pages/logout.php">Sair</a> </p>
 <?php else: ?>
     <p>Olá visitante, clique para fazer seu <a href="pages/form-login.php">Login</a></p>
 <?php endif; ?>
